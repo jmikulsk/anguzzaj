26c8a9a2386096c9069b021a7ca61da0d63417c7
KUBA <jmikulsk92>
2020-03-31 20:48:32 +0200

    make it better

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/shop'));
+app.use(express.static('./dist/anguzzaj'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/shop/'}),
+    res.sendFile('index.html', {root: 'dist/anguzzaj/'}),
 );
 
 app.listen(process.env.PORT || 8080);
